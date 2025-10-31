06a835a6d41594f2828af6712ba9a7786b85b475
Ankia-Fuls <ankiafuls01@gmail.com>
2025-10-31 14:50:48 +0200

    Image selection for lightbox added

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,9 +1,69 @@
+import { useState } from "react";
+
 function Main({ pageInert }) {
+
+    const images = [
+        {
+            id: 0,
+            src: "./images/image-product-1.jpg",
+            alt: "First image of the product"
+        },
+        {
+            id: 1,
+            src: "./images/image-product-2.jpg",
+            alt: "Second image of the product"
+        },
+        {
+            id: 2,
+            src: "./images/image-product-3.jpg",
+            alt: "Third image of the product"
+        },
+        {
+            id: 3,
+            src: "./images/image-product-4.jpg",
+            alt: "Fourth image of the product"
+        }
+    ]
+
+    const [clickedImg, setClickedImg] = useState(null);
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const setChosenPicture = (e) => {
+        const selectedId = e.target.id;
+        setCurrentIndex(selectedId);
+
+        const allThumbnails = e.target.parentNode.parentNode.children;
+
+        for (let i = 0; i < 4; i++) {
+
+            allThumbnails[i].children[0].className = "gallery__thumbnail";
+        }
+
+        e.target.className = "gallery__thumbnail gallery__thumbnail--chosen";
+    }
 
     return (
         <main inert={pageInert}>
 
-            Sneaker Company
+            <section className="gallery">
+                <div className="gallery__main">
+                    <img src={images[currentIndex].src} alt={images[currentIndex].alt} />
+                </div>
+                <div className="gallery__previews">
+                    {images.map((item, index) => (
+                        <div key={index}>
+                            <img
+                                src={item.src} alt={item.alt} id={index}
+                                className={currentIndex === index ? "gallery__thumbnail gallery__thumbnail--chosen" : "gallery__thumbnail"}
+                                onClick={(e) => { setChosenPicture(e) }}
+                            />
+                        </div>
+                    )
+                    )}
+                </div>
+            </section>
+
+            {/* Sneaker Company
 
             Fall Limited Edition Sneakers
 
@@ -15,7 +75,7 @@ function Main({ pageInert }) {
             $250.00
 
             0
-            Add to cart
+            Add to cart */}
 
         </main>
     );
